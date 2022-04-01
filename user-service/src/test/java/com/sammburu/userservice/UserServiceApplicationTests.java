024f4cb5b6f15161ca5cd24094b943f6de7516e9
mburusam <80331428+mburusam@users.noreply.github.com>
2022-04-01 23:31:52 +0300

    resilience4j commit uploads

---
+++ user-service/src/test/java/com/sammburu/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sammburu.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
